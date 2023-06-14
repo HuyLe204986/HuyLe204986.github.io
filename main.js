0e8df4825755c95845cf632c5a66114842291c23
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2023-06-14 14:28:21 +0700

    Add new project UIUX

---
+++ main.js
@@ -0,0 +1,53 @@
+var logIn = document.querySelector('.js-login');
+var signUp = document.querySelector('.js-signup');
+var modal = document.querySelector('.js-modal');
+var modalContainer = document.querySelector('.js-modal-container')
+
+var formLogIn = document.querySelector('.auth-form:last-child');
+var formSignUp = document.querySelector('.auth-form:first-child');
+
+
+var isLogIn ;
+var isSignUp;
+
+function showLogIn() {
+    modal.classList.remove('close');
+    formSignUp.classList.add('close');
+    isLogIn = true;
+    isSignUp = false;
+}
+
+function showSignUp() {
+    modal.classList.remove('close');
+    formLogIn.classList.add('close');
+    isLogIn = false;
+    isSignUp = true;
+}
+
+logIn.addEventListener('click', showLogIn);
+signUp.onclick = showSignUp;
+
+function hide() {
+    modal.classList.add('close'); 
+    if(!isLogIn && isSignUp) {
+        formLogIn.classList.remove('close');
+    }else if(!isSignUp && isLogIn) {
+        formSignUp.classList.remove('close');
+    }
+}
+
+var closeForms = document.querySelectorAll('.js-auth-form__close');
+
+for (var key in closeForms) {
+    closeForms[key].onclick = hide;
+}
+
+
+modal.addEventListener('click',hide);
+
+modalContainer.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
+
+
+
